79f3fa62862d98dddf739d81a31d0fdeca032b2e
ricardoracki <36777788+ricardoracki@users.noreply.github.com>
2018-05-05 14:52:46 -0300

    Add files via upload

--- js/01.js
+++ js/01.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
 		 var itensbrinde;
 		 var creditos;
 		 var brindeAdicoinal;
+		 var bonus;
 		 
 		 var evilMaineShild = criarItem("Escudo Evill Maine", "img/itens/hell maine shild.png" );
 		 var asaNovato = criarItem("Asa Novato", "img/itens/Asa Novato.png");
@@ -28,32 +29,44 @@ $(document).ready(function(){
 		 */
 		if (valordig >0){
 
-		 	if ( valordig < 20) {
-		 	itensbrinde = evilMaineShild;
+		 	if ( valordig < 20 && valordig < 49) {
+		 	//itensbrinde = evilMaineShild;
 		 	pacote = 1;
-		 	fator  = 2;
+		 	bonus = 10;
 		 	
 			}
 
-			  if (valordig > 20) {
+			  if (valordig > 50 && valordig < 99) {
+		 	itensbrinde = evilMaineShild;
+		 	pacote = 2;
+		 	bonus  = 20;
+		 	
+		 	}
+
+		 		if (valordig > 100) {
 		 	itensbrinde = evilMaineShild + asaNovato;
 		 	pacote = 2;
-		 	fator  = 3;
+		 	bonus  = 50;
 		 	
 		 	}
 		 
-		    creditos = (valordig * fator);
+		    creditos = (+valordig) + (+bonus);
 
-		   var mensagem1 = "Promoção "+ promoname + "<br />Válida até " + promovalidate + "<br>  Receberá " + creditos + " gold's<br>";
+		   var mensagem1 = "Promoção "+ promoname + "<br />Válida até " + promovalidate + "<br>  Receberá " + creditos + " Créditos<br> Promoção válida somente para PagSeguro";
 			
 			//$("#valordig").val(''); // limpa edit
 			$("#display2").html(mensagem1)
+
+			
+			if (itensbrinde !== undefined){
 
 			var brindes = "<ul class = 'listaitens'>"+itensbrinde+"</ul>"; //texto de brindes
 
 			$("#display3").html(brindes); //mostra itens brinde
 
-			
+			} else {
+				$("#display3").html('')
+			}
 
 
 
